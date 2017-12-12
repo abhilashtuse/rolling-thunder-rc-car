bd5ee9c1801604000811f33190d9ffa117219cea
Vishal <vishalrajendra.shrivastava@sjsu.edu>
2017-12-11 23:27:23 -0800

    Normalize GPS bearing value

--- L5_Application/source/geo_controller.cpp
+++ L5_Application/source/geo_controller.cpp
@@ -57,7 +57,9 @@ double GeoController::CalculateBearingAngle(GeoGPS geo_gps)
             (cos(gps_latitude) * sin(latitude)) -
             sin(gps_latitude) * cos(latitude) * cos(diff_longitude));
 
-    return (bearing * 180)/M_PI;
+    bearing = (bearing * 180)/M_PI;
+    bearing = fmod(bearing + 360, 360); // Normalize (0 to 360)
+    return bearing;
 }
 
 /*
